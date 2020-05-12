f38e0508d9707da3d944b77985979f240e6f0e1b
asutosh-max <56363505+asutosh-max@users.noreply.github.com>
2020-05-12 15:56:01 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import Charts from './tracker/charts/charts';
-
-ReactDom.render(
-  <Charts />,
-  document.getElementById('root')
-);
